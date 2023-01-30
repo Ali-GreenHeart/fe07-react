f2fa0cce40a5386d9ddc3d31a7bd94fbe2904329
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-01-30 18:45:08 +0400

    add getbreeds by reducer

--- src/pages/CatBreeds.jsx
+++ src/pages/CatBreeds.jsx
@@ -1,4 +1,5 @@
 import axios from "axios"
+import { useRef } from "react"
 import { useEffect } from "react"
 import { useReducer } from "react"
 
@@ -8,6 +9,8 @@ const reducer = (state, action) => {
             return action.payload
         case 'RemoveElement':
             return state.filter((cat) => cat.breed !== action.payload)
+        case "FilterElementByCountry":
+            return state.filter((cat) => cat.country.startsWith(action.payload))
         default:
             return state;
     }
@@ -15,22 +18,45 @@ const reducer = (state, action) => {
 
 const CatBreeds = ({ }) => {
     const [breeds, dispatch] = useReducer(reducer, [])
-
+    let inputRef = useRef();
     useEffect(() => {
         axios.get('https://catfact.ninja/breeds?limit=99').then(({ data }) => {
             dispatch({ type: 'GetBreeds', payload: data.data })
         })
     }, [])
 
+    useEffect(() => {
+        if (breeds.length === 0) {
+            axios.get('https://catfact.ninja/breeds?limit=99').then(({ data }) => {
+                dispatch({ type: 'GetBreeds', payload: data.data })
+            })
+            inputRef.current.value = ''
+        }
+    }, [breeds])
+
+
     return (
         <>
+
+            <input
+                ref={inputRef}
+                onChange={(e) => {
+                    dispatch({ type: 'FilterElementByCountry', payload: e.target.value })
+                    if (!e.target.value) {
+                        axios.get('https://catfact.ninja/breeds?limit=99').then(({ data }) => {
+                            dispatch({ type: 'GetBreeds', payload: data.data })
+                        })
+                    }
+                }}
+                type="text" style={{ display: 'block', width: '400px', margin: "20px auto", outline: 'none', fontSize: 32 }} />
+
             {
                 breeds.map(({ breed, coat, country, origin, pattern }) => {
                     return <div
-                        onClick={() => {
+                        key={breed} style={{ border: '2px solid', padding: 10, margin: "10px auto", maxWidth: 300, textAlign: 'center' }}>
+                        <button onClick={() => {
                             dispatch({ type: 'RemoveElement', payload: breed })
-                        }}
-                        key={breed} style={{ border: '2px solid', padding: 10, margin: 10 }}>
+                        }} style={{ float: 'right' }}>x</button>
                         <h1>{breed}</h1>
                         <h2>{country}</h2>
                         <p>{coat}</p>
